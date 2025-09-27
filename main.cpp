c181c05da49b7e3f2886b6aa9a996b5316417b6d
MrYaz <LehaHotPlay@yandex.ru>
2025-09-27 16:40:29 +0500

    Добавленно основной макет меню в основную программу

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <string>
+#include "headers/menu_class.h"
 
 using std::cout;
 using std::endl;
 
 int main()
 {
-	
+    std::string points_menu[] {
+        "Установка задач на текущий день", 
+        "Установка задач на последующие дни",
+        "Питомец",
+        "Настройка" 
+    };
+	Menu main_menu {points_menu, std::size(points_menu)};
+
+    main_menu.show();
+    main_menu.select_point();
 }
